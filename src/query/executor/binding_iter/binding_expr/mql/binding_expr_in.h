cdf2102162c27edb6a27e565abbcc87b2bf1c51e
Vergaraaa1 <133895253+Vergaraaa1@users.noreply.github.com>
2025-07-17 15:26:14 -0400

    Filter edges in MQL IN evaluation

--- src/query/executor/binding_iter/binding_expr/mql/binding_expr_in.h
+++ src/query/executor/binding_iter/binding_expr/mql/binding_expr_in.h
@@ -18,15 +18,18 @@ public:
 
     ObjectId eval(const Binding& binding) override
     {
-        auto lhs_oid    = lhs->eval(binding);
+        auto lhs_oid = lhs->eval(binding);
         auto lhs_generic = lhs_oid.id & ObjectId::GENERIC_TYPE_MASK;
-        auto lhs_type    = lhs_oid.id & ObjectId::TYPE_MASK;
+        auto lhs_type = lhs_oid.id & ObjectId::TYPE_MASK;
+
+        if (lhs_generic == ObjectId::MASK_EDGE || lhs_type == ObjectId::MASK_EDGE_LABEL)
+            return ObjectId::get_null();
 
         bool compatible = false;
         for (auto& expr : rhs) {
-            auto rhs_oid   = expr->eval(binding);
+            auto rhs_oid = expr->eval(binding);
             auto rhs_generic = rhs_oid.id & ObjectId::GENERIC_TYPE_MASK;
-            auto rhs_type    = rhs_oid.id & ObjectId::TYPE_MASK;
+            auto rhs_type = rhs_oid.id & ObjectId::TYPE_MASK;
 
             if (rhs_generic == lhs_generic && rhs_type == lhs_type) {
                 compatible = true;
