349fb3288a32ee5b6eca5af5b740ef3d3ee8b348
VicenteVicente <37163344+VicenteVicente@users.noreply.github.com>
2025-10-28 19:32:15 +0000

    Action: Update browser interface

--- browser/assets/javascript-B_bw3Rsb.js
+++ browser/assets/javascript-B_bw3Rsb.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-CaGPfIbz.js";import"./index-ySMygv2q.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-waSjb_8Q.js";import"./index-DuUc6fs4.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
