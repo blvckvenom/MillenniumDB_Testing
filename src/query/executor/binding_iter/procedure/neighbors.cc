110940853f7a9fed365f9c97483ecb3bcb601799
Andres Restrepo <an.res.sep@gmail.com>
2025-12-11 16:48:58 -0300

    add basic functionality of traversing all nodes connected by undirected edges.

--- src/query/executor/binding_iter/procedure/neighbors.cc
+++ src/query/executor/binding_iter/procedure/neighbors.cc
@@ -1,4 +1,5 @@
 #include "neighbors.h"
+#include "graph_models/gql/gql_model.h"
 
 #include <ostream>
 
@@ -17,13 +18,29 @@ void Neighbors::_begin(Binding& parent_binding_)
 
 void Neighbors::_reset()
 {
-    emit_reverse = false;
+    //emit_reverse = false;
     // aquí inicializarás el cursor de aristas cuando uses los índices reales
+    //current_record = nullptr;
+
+    std::array<uint64_t, 3> min_ids = {0, 0, 0};
+    std::array<uint64_t, 3> max_ids = {UINT64_MAX, UINT64_MAX, UINT64_MAX};
+
+    edge_iter = gql_model.n1_n2_edge->get_range(
+        &get_query_ctx().thread_info.interruption_requested,
+        min_ids,
+        max_ids
+    );
 }
 
 bool Neighbors::_next()
 {
     // TODO: implementar la iteración sobre aristas y emisión de (node, neighbor)
+    const Record<3>* current_record = edge_iter.next();
+    if (current_record) {
+        parent_binding->add(yield_vars[0], ObjectId((*current_record)[0]));
+        parent_binding->add(yield_vars[1], ObjectId((*current_record)[1]));
+        return true;
+    }
     return false;
 }
 
--- src/query/executor/binding_iter/procedure/neighbors.h
+++ src/query/executor/binding_iter/procedure/neighbors.h
@@ -4,6 +4,8 @@
 
 #include "query/executor/binding_iter.h"
 #include "query/var_id.h"
+#include "storage/index/bplus_tree/bplus_tree.h"
+#include "storage/index/record.h"
 
 namespace Procedure {
 
@@ -22,8 +24,10 @@ private:
     const std::vector<VarId> yield_vars;
     Binding* parent_binding;
     // campos para cursor de aristas y manejo de “segunda pasada” irán aquí
-    bool emit_reverse = false;
+    //bool emit_reverse = false;
     // añade aquí el tipo de iterador sobre aristas cuando lo definas
+    BptIter<3> edge_iter; // ejemplo de iterador sobre BPlusTree de 3 campos
+    //const Record<3>* current_record = nullptr;
 };
 
 } // namespace Procedure
