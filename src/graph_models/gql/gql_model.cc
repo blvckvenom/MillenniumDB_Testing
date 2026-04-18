7cea4489a5f89b05b6afaed0e19034795eda482a
Benito <95387636+blvckvenom@users.noreply.github.com>
2026-04-17 22:21:01 -0400

    feat(gql): add gnn_checkpoint_exists procedure

--- src/graph_models/gql/gql_model.cc
+++ src/graph_models/gql/gql_model.cc
@@ -27,6 +27,7 @@
 #include "query/procedure/builtin/gnn_train_procedure.h"
 #include "query/procedure/builtin/gnn_predict_procedure.h"
 #include "query/procedure/builtin/gnn_list_checkpoints_procedure.h"
+#include "query/procedure/builtin/gnn_checkpoint_exists_procedure.h"
 #endif
 #include "query/query_context.h"
 #include "storage/index/bplus_tree/bplus_tree.h"
@@ -81,6 +82,9 @@ std::unique_ptr<ModelDestroyer> GQLModel::init(const std::string& db_folder)
 
     // Register GNN checkpoint listing procedure
     catalog.register_procedure(std::make_unique<GQL::Procedures::GnnListCheckpointsProcedure>());
+
+    // Register GNN checkpoint existence-check procedure
+    catalog.register_procedure(std::make_unique<GQL::Procedures::GnnCheckpointExistsProcedure>());
 #endif
 
     return std::make_unique<ModelDestroyer>([]() { gql_model.~GQLModel(); });
